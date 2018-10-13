cbd0312975d8d1b9134fb9e59e130d429ed00fac
VladozPlay <vladozplay@gmail.com>
2018-10-13 19:01:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ client.login(process.env.TOKEN);
 
 client.on("ready", () => {
 function clear_nicks(){
-    client.guilds.get('315510884334305280').members.filter(memb => memb.displayName.startsWith('!')).forEach(member => member.setNickname(member.displayName.replace(/^!+/gi, '')).catch())
+    client.guilds.get('482619342131822592').members.filter(memb => memb.displayName.startsWith('!')).forEach(member => member.setNickname(member.displayName.replace(/^!+/gi, '')).catch())
 }
 clear_nicks();
 setInterval(clear_nicks, 300000);});
@@ -15,7 +15,7 @@ client.on("guildMemberUpdate", (old_memb, new_memb) => {
 });
 
 client.on("userUpdate", (old_user, new_user) => {
-    if (client.guilds.get('315510884334305280').members.get(new_user.id).displayName.startsWith('!')) client.guilds.get('315510884334305280').members.get(new_user.id).setNickname(client.guilds.get('315510884334305280').members.get(new_user.id).displayName.replace(/^!+/gi, '')).catch();
+    if (client.guilds.get('482619342131822592').members.get(new_user.id).displayName.startsWith('!')) client.guilds.get('482619342131822592').members.get(new_user.id).setNickname(client.guilds.get('482619342131822592').members.get(new_user.id).displayName.replace(/^!+/gi, '')).catch();
 });
 
 let arr = {
